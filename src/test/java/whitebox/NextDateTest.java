0e033423c181550da96223dd363fb7e3c6159bb0
Vasco Almeida <8210711@estg.ipp.pt>
2024-10-22 16:31:43 +0100

    Iniciar Gradle e projeto

---
+++ src/test/java/whitebox/NextDateTest.java
@@ -0,0 +1,23 @@
+package whitebox;
+
+import org.junit.jupiter.api.Test;
+
+public class NextDateTest {
+
+    @Test
+    void testValidDateIncrement() {
+
+    }
+    @Test
+    void testEndOfMonthForThirtyDayMonths() {
+
+    }
+    @Test
+    void testInvalidDates() {
+
+    }
+    @Test
+    void testNextYearBoundary() {
+
+    }
+}
